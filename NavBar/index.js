ad0dc91bd8c139b15b8b79abf63936bb7e28269d
Parisheelan Negi <parisheelannegi@gmail.com>
2022-02-26 23:26:22 +0530

    nav

--- NavBar/index.js
+++ NavBar/index.js
@@ -1,9 +1,1 @@
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector("nav-links");
 
-  burger.addEventListerner("click", () => {
-    nav.classList.toggle("nav-active");
-  });
-};
-navSlide();
